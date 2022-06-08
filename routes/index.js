218888a6993707a934645afb85c9f46479f6c406
CarlaAnne <019201787i@uandi.edu.pe>
2022-06-08 13:24:11 -0700

    act

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const productosRouter = require('./productos.route')
+const categoriasRouter = require('./categorias.route')
+const tiendasRouter = require('./tiendas.route');
+
+function rutas(app){
+  app.get('/',(req,res) => {
+    res.send("Hola desde aqui api noseque - express activo - peticion get");
+  });
+  const router = express.Router();
+  app.use('/api/v1',router);
+  router.use('/productos',productosRouter);
+  router.use('/categorias',categoriasRouter);
+  router.use('/tiendas',tiendasRouter);
+}
+
+module.exports = rutas;
